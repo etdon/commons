49bc4ab3771709fe25a6992bda4cef9aebc98005
etdon <169199686+etdon@users.noreply.github.com>
2025-01-02 16:37:45 +0100

    Added enum value beautify method.

--- src/main/java/com/etdon/commons/presentation/Beautifier.java
+++ src/main/java/com/etdon/commons/presentation/Beautifier.java
@@ -1,6 +1,40 @@
 package com.etdon.commons.presentation;
 
+import com.etdon.commons.conditional.Preconditions;
+import org.jetbrains.annotations.NotNull;
+
 public final class Beautifier {
+
+    /**
+     * Converts the provided enum value to a beautified string representation, beautified in this context means that
+     * underscores get replaced with spaces and only the first letter of each word is upper case.
+     * <p>
+     * Example: ENUM_VALUE -> Enum Value
+     * </p>
+     *
+     * @param input The enum value.
+     * @return The beautified string representation.
+     */
+
+    public static String beautifyEnum(@NotNull final Enum<?> input) {
+
+        Preconditions.checkNotNull(input);
+        final char[] chars = input.name().toCharArray();
+        boolean capitalize = true;
+        for (int i = 0; i < chars.length; i++) {
+            final char currentChar = chars[i];
+            if (currentChar == '_') {
+                chars[i] = ' ';
+                capitalize = true;
+            } else {
+                chars[i] = capitalize ? Character.toUpperCase(currentChar) : Character.toLowerCase(currentChar);
+                if (capitalize) capitalize = false;
+            }
+        }
+
+        return new String(chars);
+
+    }
 
     private Beautifier() {
 
